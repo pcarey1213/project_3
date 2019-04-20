fa47fe7d482f5acc198cf5054db8724055d2becc
Jenny Na <jennyna13@gmail.com>
2019-04-20 11:25:17 -0400

    userPage

--- server/routes/userPage.js
+++ server/routes/userPage.js
@@ -2,4 +2,14 @@ const express = require('express')
 const router = express.Router()
 const dbUser = require("../database/models/user")
 
+router.route("/:id")
+.get(userPageController.findOne);
+
+router.route("/:id")
+.get(categoriesController.findOne);
+
+router.route("/:id")
+.post(userPageController.dbUser);
+
+
 module.exports = userPage
